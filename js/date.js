fffc8dc97d014e429b2f6458027eb95ccd54ab4d
Muhamad Husein <m.husein27@gmail.com>
2020-03-13 09:50:01 +0700

    Create date.js

---
+++ js/date.js
@@ -0,0 +1,11 @@
+	// Check if string is date format
+  const isDate = date => {
+    try{
+      new Date(date).toISOString();
+      return true;
+    }catch(e){ 
+      return false; 
+    }
+	}
+  
+  
